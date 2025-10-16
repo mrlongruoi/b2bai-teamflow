9ea72921521d5e4ffa925f38cdc5607f7461f6dc
mrlongruoi <mrlong01012020@gmail.com>
2025-10-16 23:29:08 +0700

    02: landing page

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,14 @@
+import { HeroHeader } from "@/app/(marketing)/_components/header";
+import HeroSection from "@/app/(marketing)/_components/hero-section";
+// import { ThemeToggle } from "@/components/ui/theme-toggle";
+
+export default function Home() {
+  
+  return (
+    <div>
+      {/* <ThemeToggle/> */}
+      <HeroHeader/>
+      <HeroSection/>
+    </div>
+  );
+}
